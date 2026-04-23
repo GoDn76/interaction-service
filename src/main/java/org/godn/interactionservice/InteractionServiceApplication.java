c41823c22e56151a9f5e211871c962c39d2b15c8
GoDn76 <godncraft7610tube@gmail.com>
2026-04-24 00:30:42 +0530

    Created a Simple CRON Job using @Scheduled Testing this

--- src/main/java/org/godn/interactionservice/InteractionServiceApplication.java
+++ src/main/java/org/godn/interactionservice/InteractionServiceApplication.java
@@ -2,8 +2,10 @@ package org.godn.interactionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class InteractionServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/godn/interactionservice/service/RedisNotificationService.java
+++ src/main/java/org/godn/interactionservice/service/RedisNotificationService.java
@@ -2,9 +2,12 @@ package org.godn.interactionservice.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @Service
@@ -27,5 +30,39 @@ public class RedisNotificationService {
             log.info("Push Notification Sent to User: {}", message);
             stringRedisTemplate.opsForValue().set(cooldownKey, "locked", cooldown);
         }
+    }
+
+    @Scheduled(fixedRate = 300000) // For 5 minutes...
+    public void sweepPendingNotifications() {
+        Set<String> queueKeys = stringRedisTemplate.keys("user:*:pending_notifs");
+
+        if(queueKeys == null || queueKeys.isEmpty()) {
+            return;
+        }
+
+        log.info("CRON Sweeper started. Checking pending notifications for {} users.", queueKeys.size());
+
+        for (String queueKey : queueKeys) {
+            List<String> messages = stringRedisTemplate.opsForList().range(queueKey, 0, -1); // Getting all the messages...
+
+            if (messages != null && !messages.isEmpty()) {
+                int totalPending = messages.size();
+                String firstMessage = messages.get(0);
+
+                String firstBotId = firstMessage.split(" ")[1];
+                int othersCount = totalPending - 1;
+
+                if (othersCount > 0) {
+                    log.info("Summarized Push Notification: Bot {} and {} others interacted with your posts.", firstBotId, othersCount);
+                } else {
+                    log.info("Summarized Push Notification: {}", firstMessage);
+                }
+
+                // Remove from Redis...
+                stringRedisTemplate.delete(queueKey);
+            }
+        }
+        log.info("Sweeping complete.");
+
     }
 }
